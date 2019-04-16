3348e2adc72d337aead3a1e867038e3f262dd04c
洪远洋 <hongyuanyang@huobi.com>
2019-04-16 18:21:00 +0800

    notify future

--- src/main.rs
+++ src/main.rs
@@ -123,6 +123,7 @@ impl Future for WaitForIt {
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         let now = Utc::now();
+        thread::sleep(::std::time::Duration::from_millis(500));
         if self.until < now {
             Ok(Async::Ready(
                 format!("{} after {} polls!", self.message, self.polls),
@@ -138,13 +139,72 @@ impl Future for WaitForIt {
         }
     }
 }
+use std::thread;
+pub struct WaitInAnotherThread {
+    end_time: DateTime<Utc>,
+    running: bool,
+}
+
+impl WaitInAnotherThread {
+    pub fn
+    new(how_long: Duration) -> WaitInAnotherThread {
+        WaitInAnotherThread {
+            end_time: Utc::now() + how_long,
+            running: false,
+        }
+    }
+
+    fn run(&mut self, task: task::Task) {
+        let lend = self.end_time;
+
+        thread::spawn(move || {
+            while Utc::now() < lend {
+                let delta_sec = lend.timestamp() - Utc::now().timestamp();
+                if delta_sec > 0 {
+                    thread::sleep(::std::time::Duration::from_secs(delta_sec as u64));
+                }
+                task.notify();
+            }
+            println!("the time has come == {:?}!", lend);
+        });
+    }
+}
+
+use futures::task;
+
+impl Future for WaitInAnotherThread {
+    type Item = ();
+    type Error = Box<Error>;
 
 
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        if Utc::now() < self.end_time {
+            println!("not ready yet! parking the task.");
+
+            if !self.running {
+                println!("side thread not running! starting now!");
+                self.run(task::current());
+                self.running = true;
+            }
+
+            Ok(Async::NotReady)
+        } else {
+            println!("ready! the task will complete.");
+            Ok(Async::Ready(()))
+        }
+    }
+}
 
 use futures::future::join_all;
 fn main() {
 
     let mut reactor = Core::new().unwrap();
+
+    let wiat = WaitInAnotherThread::new(Duration::seconds(3));
+    println!("wait future started");
+    let ret = reactor.run(wiat).unwrap();
+    println!("wait future completed. ret == {:?}", ret);
 
     let wfi_1 = WaitForIt::new("I'm done:".to_owned(), Duration::seconds(1));
     println!("wfi_1 == {:?}", wfi_1);
