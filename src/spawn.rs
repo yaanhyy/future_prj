7cda58e5a7356933ea0b163d48a45a619bb2b912
yaanhyy <yaanhyy@gmail.com>
2019-08-19 01:22:22 +0800

    add mpsc oneshot

--- src/spawn.rs
+++ src/spawn.rs
@@ -1,7 +1,9 @@
 extern crate tokio;
 extern crate futures;
-
+use futures::sync::oneshot;
 use futures::future::lazy;
+use futures::sync::mpsc;
+use futures::{stream, Future, Stream, Sink};
 
 fn spawn_task() {
     (0..4).flat_map(|x| x * 100 .. x * 110)
@@ -18,6 +20,38 @@ fn spawn_task() {
         }
 
         Ok(())
+    }));
+
+    tokio::run(lazy(|| {
+        let (tx, rx) = oneshot::channel();
+
+        tokio::spawn(lazy(|| {
+            tx.send("hello from spawned task");
+            Ok(())
+        }));
+
+        rx.and_then(|msg| {
+            println!("Got `{}`", msg);
+            Ok(())
+        })
+            .map_err(|e| println!("error = {:?}", e))
+    }));
+
+    tokio::run(lazy(|| {
+        let (tx, rx) = mpsc::channel(1_024);
+
+        tokio::spawn({
+            stream::iter_ok(0..10).fold(tx, |tx, i| {
+                tx.send(format!("Message {} from spawned task", i))
+                    .map_err(|e| println!("error = {:?}", e))
+            })
+                .map(|_| ()) // Drop tx handle
+        });
+
+        rx.for_each(|msg| {
+            println!("Got `{}`", msg);
+            Ok(())
+        })
     }));
 }
 
